a2e16758b275021cd49fcfdffec82e0fed7a184d
imadsyed333 <creeperone23@gmail.com>
2026-03-20 02:22:18 -0400

    Removed client-side logic to convert to server components, reducing render times on frontend

--- app/title/[titleId]/page.tsx
+++ app/title/[titleId]/page.tsx
@@ -1,31 +1,28 @@
-"use client";
-
-import { fetchAnalysis } from "@/lib/api/analyzer";
 import { fetchMovie } from "@/lib/api/imdbapi";
 import { ImdbapiTitle } from "@/lib/types/imdbapi";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
-const MoviePage = () => {
-  const params = useParams<{ titleId: string }>();
+type paramsType = { titleId: string };
+
+const MoviePage = async ({ params }: { params: paramsType }) => {
   const id = params.titleId;
 
-  const [movie, setMovie] = useState<ImdbapiTitle>();
-
-  useEffect(() => {
-    if (id) {
-      fetchMovie(id)
-        .then((res) => setMovie(res))
-        .catch((err) => console.error(err));
-    }
-  }, [id]);
+  const movie: ImdbapiTitle = await fetchMovie(id);
 
   return (
     <div className="flex h-screen justify-center items-center text-black">
       <div className="flex flex-row h-100">
         <img src={movie?.primaryImage?.url} />
         <div className="text-3xl ml-2">{movie?.primaryTitle}</div>
+        <div className="text-2xl mt-4">
+          {movie?.rating?.aggregateRating
+            ? `Rating: ${movie.rating.aggregateRating} (${movie.rating.voteCount} votes)`
+            : "No rating available"}
+        </div>
+        <div className="text-xl mt-4">{movie?.plot}</div>
       </div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Generate Analysis
+      </button>
     </div>
   );
 };
--- components/ui/searchtitlecard.tsx
+++ components/ui/searchtitlecard.tsx
@@ -1,7 +1,5 @@
-"use client";
 import { ImdbapiTitleSearch } from "@/lib/types/imdbapi";
 import Link from "next/link";
-import React from "react";
 
 type SearchTitleCardProps = {
   searchTitle: ImdbapiTitleSearch;
